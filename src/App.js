0a5bbf9d206e47d215ef5fc1cf18a21c3d2e3ba5
Fran1089 <Franciscogomez2k14@gmail.com>
2022-03-21 08:53:33 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
+import SearchIcon from './search.svg'
+import MovieCart from "./movie_cart.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//eff53899
+
+
+const API_URL = 'http://www.omdbapi.com?apiKey=eff53899'
+
+const App = () => {
+
+    const [movies, setMovies] = useState([]);  
+    const [searchTerm, setSearchTerm] = useState([]);
+
+    useEffect(() => {
+        searchMovies('Endgame');
+    }, []);
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+
+        setMovies(data.Search);
+    }
+
+    const movie1 = {
+        "Title": "Avengers: Endgame",
+        "Year": "2019",
+        "imdbID": "tt4154796",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg"
+    }
+
+    return(
+        <div className="app">
+            <h1>Movieland</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Buscar pelicula"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {movies?.length > 0
+                ? (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCart movie={movie} />
+                        ))}
+                    </div>
+                ) : (
+                    
+                    <div className="empty">
+                        <h2>No movies found</h2>
+                    </div>
+                )
+            }
+
+        </div>
+    );
 }
 
-export default App;
+export default App; 
